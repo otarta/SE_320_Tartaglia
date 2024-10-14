6013980edfe5346117cf37cb2af13e6af1870e89
otarta <157425197+otarta@users.noreply.github.com>
2024-10-14 19:34:59 -0400

    HW2 updated

--- HW2/HW2/src/HW2.java
+++ HW2/HW2/src/HW2.java
@@ -1,11 +1,18 @@
 import java.util.ArrayList;
+import java.util.Scanner;
 public class HW2 {
-    public static void main(String[] args) throws Exception {
-       
+    public static void main(String[] args) throws Exception 
+    {
+        
+    }
         //1
+        //in the GenericStack Class 
+
+
 
         //2
-
+        public void question2()
+        {
         ArrayList<Integer> list = new ArrayList<>();
         list.add(14);
         list.add(24);
@@ -29,12 +36,41 @@ public class HW2 {
             }
             return newList;
         }
+        
         //3
+        public void question3()
+        {
+            Scanner scannerA = new Scanner(System.in);
+            try
+            {
+            int inputNumber = scannerA.nextInt();
+
+            assert (inputNumber >= 0 && inputNumber<=10) : "The number is out of range" ;
+
+            System.out.println("The inputted number is: "+inputNumber);
+            }
+            catch(Exception e)
+            {
+                System.out.println("An error occurred. Please try again");
+                question3();
+            }
+            finally
+            {
+                scannerA.close();
+            }
+        }
+
+
+
+    }        
+       
 
 
 
 
 
 
-    }
+
+
+    
 
