1844b6236cd09a9727c7c83305d1d15800b18493
Armando Tartaglia <tartaga1@my.erau.edu>
2024-10-29 13:34:01 -0400

    hw4 start

---
+++ HW4/src/HW4.java
@@ -0,0 +1,45 @@
+import java.util.HashSet;
+public class HW4 {
+    public static void main(String[] args) throws Exception 
+    {
+
+    }
+
+
+
+    public void Question1()
+    {
+
+        HashSet<String> hash1 = new HashSet<String>();
+        HashSet<String> hash2 = new HashSet<String>();
+
+        hash1.add("George");
+        hash1.add("Jim");
+        hash1.add("John");
+        hash1.add("Blake");
+        hash1.add("Kevin");
+        hash1.add("Michael");
+
+        hash2.add("George");
+        hash2.add("Katie");
+        hash2.add("Kevin");
+        hash2.add("Michelle");
+        hash2.add("Ryan");
+
+
+
+
+
+
+
+
+    }
+
+
+
+
+
+
+
+
+}
